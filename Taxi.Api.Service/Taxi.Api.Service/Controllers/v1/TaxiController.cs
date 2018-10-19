a174c6971ae747b17098d606b5923aa6526a87c5
Valeriy Abakumov <VIAbakumov@alfa-bank.ru>
2018-10-19 14:33:10 +0300

    Добавлен контроллер для заказа такси

---
+++ Taxi.Api.Service/Taxi.Api.Service/Controllers/v1/TaxiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Taxi.Api.Service.Controllers.v1
+{
+    [Produces("application/json")]
+    [Route("api/v1/[controller]")]
+    public class TaxiController : Controller
+    {
+        [HttpPost("make-order")]
+        public IActionResult MakeOrder([FromBody]MakeOrderTaxiModel model)
+        {
+            return
+            View();
+        }
+    }
+}
