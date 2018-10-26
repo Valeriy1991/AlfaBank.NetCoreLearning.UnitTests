095490cb697fdbc412e9a6452cd09caa1e3e4219
Valeriy Abakumov <VIAbakumov@alfa-bank.ru>
2018-10-26 13:31:18 +0300

    Добавлена модель с ошибками

---
+++ src/Core.Models/Common/ErrorModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Core.Models.Common
+{
+    [DataContract]
+    public class ErrorModel
+    {
+        [DataMember(Name = "isError")]
+        public bool IsError => true;
+
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+    }
+}
