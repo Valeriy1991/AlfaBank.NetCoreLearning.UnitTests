a4066d71705efbe9f17caf3ee84b9ce3637ac002
Valeriy Abakumov <VIAbakumov@alfa-bank.ru>
2018-10-29 13:07:49 +0300

    Фейк-генератор переписан на правила Bogus

--- src/Core.Models/ApiModels/Fakes/MakeOrderModelFake.cs
+++ src/Core.Models/ApiModels/Fakes/MakeOrderModelFake.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Bogus;
 
 namespace Core.Models.ApiModels.Fakes
@@ -7,6 +9,7 @@ namespace Core.Models.ApiModels.Fakes
     [ExcludeFromCodeCoverage]
     public static class MakeOrderModelFake
     {
+        /*
         private static readonly Faker Faker = new Faker();
 
         public static MakeOrderModel Generate()
@@ -19,6 +22,24 @@ namespace Core.Models.ApiModels.Fakes
                 Phone = Faker.Phone.PhoneNumber("+7-###-###-##-##"),
                 When = Faker.Date.Future()
             };
+        }
+        */
+        private static readonly Faker<MakeOrderModel> Faker =
+                new Faker<MakeOrderModel>()
+                    .RuleFor(e => e.From, f => f.Address.FullAddress())
+                    .RuleFor(e => e.To, f => f.Address.FullAddress())
+                    .RuleFor(e => e.Comments, f => f.Random.Words())
+                    .RuleFor(e => e.Phone, f => f.Phone.PhoneNumber("+7-9##-###-##-##"))
+                    .RuleFor(e => e.When, f => f.Date.Future())
+            ;
+
+        public static MakeOrderModel Generate()
+        {
+            return Faker.Generate();
+        }
+        public static List<MakeOrderModel> Generate(int count)
+        {
+            return Faker.Generate(count).ToList();
         }
     }
 }
