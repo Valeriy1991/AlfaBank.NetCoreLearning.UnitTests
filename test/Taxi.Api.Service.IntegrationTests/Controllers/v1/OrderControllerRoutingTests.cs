9b72982a109d16ae1a0e91abd35c115dcf4cb59f
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-11-29 18:31:05 +0300

    Добавлена версия интеграционного теста роутинга с файлом БД, чтобы метод действия контроллера возвращал true. Недостаток: данные реально пишутся в БД

--- test/Taxi.Api.Service.IntegrationTests/Controllers/v1/OrderControllerRoutingTests.cs
+++ test/Taxi.Api.Service.IntegrationTests/Controllers/v1/OrderControllerRoutingTests.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Core.Database;
+using Core.Database.Abstract;
+using Core.Models.ApiModels.Fakes;
+using Ether.Outcomes.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using NSubstitute;
 using Taxi.Api.Service.Controllers.v1;
 using Xunit;
 
@@ -7,8 +17,27 @@ namespace Taxi.Api.Service.IntegrationTests.Controllers.v1
 {
     [ExcludeFromCodeCoverage]
     [Trait("Category", "Integration - Routing")]
-    public class OrderControllerRoutingTests
+    public class OrderControllerRoutingTests : RoutingTest
     {
-        
+        public OrderControllerRoutingTests(WebApplicationFactory<Startup> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Post__MakeNewOrder()
+        {
+            // Arrange
+            var makeOrderModel = MakeOrderModelFake.Generate();
+            var json = JsonConvert.SerializeObject(makeOrderModel);
+            var url = "/api/v1/order/make";
+            var client = WebApplicationFactory.CreateClient();
+            var httpContent = CreateHttpContent(json);
+            // Act
+            var response = await client.PostAsync(url, httpContent);
+            var makeOrderResultAsJson = await response.Content.ReadAsStringAsync();
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Contains("\"success\":true", makeOrderResultAsJson);
+        }
     }
 }
