b5b12597a4d1388e7c9875820f093645b39718b3
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-11-30 11:48:31 +0300

    Создан проект с тестами моделей + первый тест на сериализацию json

---
+++ test/Core.Models.Tests/Json/DriverTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Core.Models.Fake;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Core.Models.Tests.Json
+{
+    [ExcludeFromCodeCoverage]
+    [Trait("Category", "Integration - Json")]
+    public class DriverTests
+    {
+        [Fact]
+        public void ToJson__AllSimplePropertiesIsValid()
+        {
+            // Arrange
+            var model = DriverFake.Generate();
+            // Act
+            var jsonString = JsonConvert.SerializeObject(model);
+            // Assert
+            dynamic json = JObject.Parse(jsonString);
+            Assert.Equal(model.Id, (int)json["id"]);
+            Assert.Equal(model.FullName, (string)json["name"]);
+            Assert.Equal(model.Phone, (string)json["phone"]);
+        }
+    }
+}
