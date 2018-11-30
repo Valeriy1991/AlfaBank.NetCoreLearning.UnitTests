d6d2088a9666dd5c90384ed2eb40eef1807c50a7
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-11-30 12:54:19 +0300

    Добавлен тест на проверку navigation property "Orders" для сущности Driver

--- test/Core.Models.Tests/Json/DriverTests.cs
+++ test/Core.Models.Tests/Json/DriverTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Core.Models.Fake;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -22,6 +23,21 @@ namespace Core.Models.Tests.Json
             Assert.Equal(model.Id, (int)json["id"]);
             Assert.Equal(model.FullName, (string)json["fullName"]);
             Assert.Equal(model.Phone, (string)json["phone"]);
+        }
+
+        [Fact]
+        public void ToJson__OrdersContainsValidItems()
+        {
+            // Arrange
+            var driver = DriverFake.Generate();
+            driver.Orders = OrderFake.Generate(3);
+            var jsonString = JsonConvert.SerializeObject(driver);
+            // Act
+            dynamic json = JObject.Parse(jsonString);
+            // Assert
+            var orders = (List<Order>)json["orders"].ToObject<List<Order>>();
+            Assert.Equal(driver.Orders.Count, orders.Count);
+            Assert.DoesNotContain(orders, e => e == null);
         }
     }
 }
