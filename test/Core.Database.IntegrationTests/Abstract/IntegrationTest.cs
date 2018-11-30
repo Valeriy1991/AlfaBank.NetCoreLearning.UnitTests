78ac579c8fdb8020af8b795c1cd71b3174871387
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-11-30 13:58:23 +0300

    Добавлен базовый класс для интеграционных тестов БД

---
+++ test/Core.Database.IntegrationTests/Abstract/IntegrationTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Core.Database.DbExecutors;
+using Core.Models.Settings;
+
+namespace Core.Database.IntegrationTests.Abstract
+{
+    [ExcludeFromCodeCoverage]
+    public abstract class IntegrationTest
+    {
+        public AppSettings AppSettings { get; set; }
+
+        protected IntegrationTest()
+        {
+            AppSettings = new AppSettings()
+            {
+                ConnectionStrings = new ConnectionStringSettings()
+                {
+                    OrdersDb = @"Data Source=D:\Projects\Learn\NetCore\orders.db"
+                }
+            };
+        }
+
+        protected OrderContext CreateDbContext()
+        {
+            var orderDbContextFactory = new OrdersDbContextFactory();
+            return orderDbContextFactory.Create(AppSettings.ConnectionStrings.OrdersDb);
+        }
+        protected OrderContext CreateTransactionalDbContext()
+        {
+            var orderDbContextFactory = new OrdersDbContextFactory();
+            var dbContext = orderDbContextFactory.Create(AppSettings.ConnectionStrings.OrdersDb);
+            dbContext.Database.BeginTransaction();
+            return dbContext;
+        }
+    }
+}
