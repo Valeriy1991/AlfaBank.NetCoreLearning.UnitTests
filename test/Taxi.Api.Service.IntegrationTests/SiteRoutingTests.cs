7cf661291093ce2525d09e8cd9312a4902a850c7
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-11-30 10:59:41 +0300

    Пока что тест роутинга главной страницы сделан отдельно, без базового класса RoutingTest

--- test/Taxi.Api.Service.IntegrationTests/SiteRoutingTests.cs
+++ test/Taxi.Api.Service.IntegrationTests/SiteRoutingTests.cs
@@ -2,16 +2,20 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Taxi.Api.Service.IntegrationTests.Infrastructure;
 using Xunit;
 
 namespace Taxi.Api.Service.IntegrationTests
 {
     [ExcludeFromCodeCoverage]
     [Trait("Category", "Integration - Routing")]
-    public class SiteRoutingTests : RoutingTest
+    public class SiteRoutingTests : IClassFixture<WebApplicationFactory<Startup>>
     {
-        public SiteRoutingTests(WebApplicationFactory<Startup> factory) : base(factory)
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public SiteRoutingTests(WebApplicationFactory<Startup> factory)
         {
+            _factory = factory;
         }
 
         [Theory]
@@ -19,7 +23,7 @@ namespace Taxi.Api.Service.IntegrationTests
         public async Task Get_ClientReturnSuccess(string url)
         {
             // Arrange
-            var client = WebApplicationFactory.CreateClient();
+            var client = _factory.CreateClient();
             // Act
             var response = await client.GetAsync(url);
             // Assert
