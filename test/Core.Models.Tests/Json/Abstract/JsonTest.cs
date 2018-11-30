ce0abf7cea992c46feea7a50054c9ddce0d86670
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-11-30 11:56:57 +0300

    Добавлен базовый класс JsonTest с 1 методом, который сериализует в Json с форматированием "pretty-print"

---
+++ test/Core.Models.Tests/Json/Abstract/JsonTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace Core.Models.Tests.Json.Abstract
+{
+    [ExcludeFromCodeCoverage]
+    public abstract class JsonTest
+    {
+        public string SerializeObjectToJson(object model)
+        {
+            return JsonConvert.SerializeObject(model, new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            });
+        }
+    }
+}
