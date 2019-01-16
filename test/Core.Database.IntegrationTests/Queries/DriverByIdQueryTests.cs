ab8c10b87822fdf819c556933a14649bade90123
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2019-01-16 16:47:51 +0300

    1 из интеграционных тестов с БД переделал на конструктор + IDisposable

--- test/Core.Database.IntegrationTests/Queries/DriverByIdQueryTests.cs
+++ test/Core.Database.IntegrationTests/Queries/DriverByIdQueryTests.cs
@@ -14,72 +14,74 @@ namespace Core.Database.IntegrationTests.Queries
 {
     [ExcludeFromCodeCoverage]
     [Trait("Category", "Integration - Database")]
-    public class DriverByIdQueryTests : IntegrationTest
+    public class DriverByIdQueryTests : IntegrationTest, IDisposable
     {
         private readonly Faker _faker = new Faker();
-        private readonly DriverTestRepository _driverTestRepository = new DriverTestRepository();
 
         private DriverByIdQuery CreateTestedComponent(IDbExecutor dbExecutor)
         {
             return new DriverByIdQuery(dbExecutor);
         }
 
+        private readonly DriverTestRepository _driverTestRepository = new DriverTestRepository();
+        private readonly IDbExecutor _dbExecutor;
+        private readonly DriverByIdQuery _query;
+
+        public DriverByIdQueryTests()
+        {
+            // Т.к. мы только читаем данные из БД, то не используем транзакционность
+            _dbExecutor = CreateDbExecutor();
+            _query = CreateTestedComponent(_dbExecutor);
+        }
+
         [Fact]
         public void Get__DriverDoesNotExistInStorage__ReturnNull()
         {
-            // Т.к. мы только читаем данные из БД, то не используем транзакционность
-            using (var dbExecutor = CreateDbExecutor())
-            {
-                // Arrange
-                var notExistsId = _faker.Random.Int(max: -1);
-                var query = CreateTestedComponent(dbExecutor);
-                // Act
-                var driver = query.Get(notExistsId);
-                // Assert
-                Assert.Null(driver);
-            }
+            // Arrange
+            var notExistsId = _faker.Random.Int(max: -1);
+            // Act
+            var driver = _query.Get(notExistsId);
+            // Assert
+            Assert.Null(driver);
         }
 
         [Fact]
         public void Get__DriverExistsInStorage__ReturnThisDriver_V1()
         {
-            using (var dbExecutor = CreateTransactionalDbExecutor())
-            {
-                // Arrange
-                var driver = DriverFake.Generate();
-                var insertDriverSql = $@"
+            // Arrange
+            var driver = DriverFake.Generate();
+            var insertDriverSql = $@"
 insert into Drivers(FullName, Phone)
 values (""{driver.FullName}"", ""{driver.Phone}"");
 
 select last_insert_rowid();
 ";
-                var driverId = dbExecutor.Query<int>(insertDriverSql).First();
-                var query = CreateTestedComponent(dbExecutor);
-                // Act
-                var foundDriver = query.Get(driverId);
-                // Assert
-                Assert.NotNull(foundDriver);
-                Assert.Equal(driver.Phone, foundDriver.Phone);
-                Assert.Equal(driver.FullName, foundDriver.FullName);
-            }
+            var driverId = _dbExecutor.Query<int>(insertDriverSql).First();
+            // Act
+            var foundDriver = _query.Get(driverId);
+            // Assert
+            Assert.NotNull(foundDriver);
+            Assert.Equal(driver.Phone, foundDriver.Phone);
+            Assert.Equal(driver.FullName, foundDriver.FullName);
         }
 
         [Fact]
         public void Get__DriverExistsInStorage__ReturnThisDriver_V2()
         {
-            using (var dbExecutor = CreateTransactionalDbExecutor())
-            {
-                // Arrange
-                var driver = DriverFake.Generate();
-                var driverId = _driverTestRepository.Create(dbExecutor, driver);
-                var query = CreateTestedComponent(dbExecutor);
-                // Act
-                var foundDriver = query.Get(driverId);
-                // Assert
-                Assert.NotNull(foundDriver);
-                Assert.Equal(driver.Phone, foundDriver.Phone);
-                Assert.Equal(driver.FullName, foundDriver.FullName);
-            }
+            // Arrange
+            var driver = DriverFake.Generate();
+            var driverId = _driverTestRepository.Create(_dbExecutor, driver);
+            // Act
+            var foundDriver = _query.Get(driverId);
+            // Assert
+            Assert.NotNull(foundDriver);
+            Assert.Equal(driver.Phone, foundDriver.Phone);
+            Assert.Equal(driver.FullName, foundDriver.FullName);
+        }
+
+        public void Dispose()
+        {
+            _dbExecutor?.Dispose();
         }
     }
 }
